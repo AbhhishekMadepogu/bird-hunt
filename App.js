de583ed5583b99c428eeb743bb7936908afd0305
Pat abhishek <patabhishek@Pats-MacBook-Air.local>
2022-09-01 14:09:39 -0400

    added game engine and matter

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
 
 export default function App() {
   return (
     <View style={{flex:1}}>
-      
+      <GameEngine>
+        
+      </GameEngine>
       <StatusBar style="auto" hidden={true} />
     </View>
   );
